53369cd05bc3f6a2f612d568a51b7795d1441854
wovlf02 <nskfn02@gmail.com>
2025-01-20 17:36:23 +0900

    back add

---
+++ back/src/main/java/com/studymate/back/entity/User.java
@@ -0,0 +1,60 @@
+package com.studymate.back.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@Setter
+public class User {
+
+    /**
+     * 사용자 고유 ID -> 회원번호
+     * Primary Key
+     * Auto Increment
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 아이디
+     * 고유값 -> 회원가입 시 중복확인
+     * Not Null
+     */
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    /**
+     * 암호화된 비밀번호
+     * Not Null
+     */
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    /**
+     * 사용자 이름
+     * Not Null
+     */
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    /**
+     * 전화번호
+     * Not Null
+     */
+    @Column(nullable = false, length = 15)
+    private String phone;
+
+    /**
+     * 이메일
+     * Unique
+     * Not Null
+     */
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+}
